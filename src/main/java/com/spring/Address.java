511dadbfbbffbece83026cea6bbabbb8b43cbeef
abdul-rahman <smdarahman55@gmail.com>
2024-10-18 10:20:44 +0530

    added all files of springTAPAcad

---
+++ src/main/java/com/spring/Address.java
@@ -0,0 +1,33 @@
+package com.spring;
+
+public class Address {
+
+	private String city;
+	private String state;
+
+	public Address() {
+		System.out.println("Address() - 0");
+	}
+
+	public Address(String city, String state) {
+		this.city = city;
+		this.state = state;
+//		System.out.println("Address(params)");
+	}
+
+	@Override
+	public String toString() {
+		return "[ " + city + ", " + state + " ]";
+	}
+
+	public void setCity(String city) {
+//		System.out.println("setCity()");
+		this.city = city;
+	}
+
+	public void setState(String state) {
+//		System.out.println("setState()");
+		this.state = state;
+	}
+
+}
---
+++ src/main/java/com/tap/app/Program1.java
@@ -0,0 +1,21 @@
+package com.tap.app;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.spring.GoldCustomer;
+
+public class Program1 {
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+
+		GoldCustomer myCustomer = (GoldCustomer) context.getBean("goldCustomer");
+
+		System.out.println(myCustomer.getCustomerDetails());
+		System.out.println(myCustomer);
+
+		GoldCustomer myCustomerOne = (GoldCustomer) context.getBean("goldCustomer");
+		GoldCustomer myCustomerTwo = (GoldCustomer) context.getBean("goldCustomer");
+
+		System.out.println(myCustomerOne == myCustomerTwo);
+	}
+}
